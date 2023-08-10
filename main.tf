bf427dac6b571d7f00925a0ae93be946abb14cd1
Ruslan Riznyk <ruslan_riznyk@epam.com>
2023-08-10 11:36:33 +0300

    chore:	modified main.tf, outputs.tf

--- main.tf
+++ main.tf
@@ -19,33 +19,6 @@ locals {
   vpc_subnet_names = {
     for subnet in var.vpc_config.subnets : subnet.name => replace("${local.prefix}${local.env}${local.suffix}${subnet.name}", "_", "-")
   }
-locals {
-  prefix = length(var.prefix) == 0 ? "" : "${var.prefix}-"
-  suffix = length(var.suffix) == 0 ? "" : "-${var.suffix}"
-  env    = length(var.env) == 0 ? "" : "${var.env}-"
-
-  vpc_subnets = flatten([
-    for subnet in coalesce(var.vpc_config.subnets, []) : {
-      region                   = subnet.region
-      subnet_name              = subnet.name
-      subnet_cidr              = subnet.subnet_cidr
-      secondary_ranges         = coalesce(subnet.secondary_ranges, [])
-      private_ip_google_access = subnet.private_ip_google_access
-      log_config               = subnet.log_config
-    }
-  ])
-
-  vpc_network_name = replace("${local.prefix}${local.env}${local.suffix}${var.vpc_config.name}", "_", "-")
-
-  vpc_subnet_names = {
-    for subnet in var.vpc_config.subnets : subnet.name => replace("${local.prefix}${local.env}${local.suffix}${subnet.name}", "_", "-")
-  }
-}
-
-resource "google_compute_network" "vpc" {
-  name                    = local.vpc_network_name
-  project                 = var.project_id
-  auto_create_subnetworks = false
 }
 resource "google_compute_network" "vpc" {
   name                    = local.vpc_network_name
@@ -60,29 +33,7 @@ resource "google_compute_subnetwork" "subnet" {
   region        = each.value.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = each.value.subnet_cidr
-resource "google_compute_subnetwork" "subnet" {
-  for_each      = { for s in local.vpc_subnets : s.subnet_name => s }
-  name          = local.vpc_subnet_names[each.key]
-  project       = var.project_id
-  region        = each.value.region
-  network       = google_compute_network.vpc.name
-  ip_cidr_range = each.value.subnet_cidr
 
-  dynamic "secondary_ip_range" {
-    for_each = each.value.secondary_ranges
-    content {
-      range_name    = secondary_ip_range.value.range_name
-      ip_cidr_range = secondary_ip_range.value.range_cidr
-    }
-  }
-  private_ip_google_access = each.value.private_ip_google_access
-  dynamic "log_config" {
-    for_each = [each.value.log_config]
-    content {
-      aggregation_interval = log_config.value.aggregation_interval
-      flow_sampling        = log_config.value.flow_sampling
-      metadata             = log_config.value.metadata
-    }
   dynamic "secondary_ip_range" {
     for_each = each.value.secondary_ranges
     content {
--- outputs.tf
+++ outputs.tf
@@ -9,21 +9,9 @@ output "subnet_id" {
     for subnet in var.vpc_config.subnets : subnet.name => google_compute_subnetwork.subnet[subnet.name].id
     if var.vpc_config.subnets != null
   }
-output "vpc_name" {
-  description = "Name of the VPC network"
-  value       = google_compute_network.vpc.name
-}
-
-output "subnet_id" {
-  description = "IDs of the created subnets per VPC"
-  value = {
-    for subnet in var.vpc_config.subnets : subnet.name => google_compute_subnetwork.subnet[subnet.name].id
-    if var.vpc_config.subnets != null
-  }
-}
 
 output "subnet_name" {
-  description = "IDs of the created subnets per VPC"
+  description = "Names of the created subnets per VPC"
   value = {
     for subnet in var.vpc_config.subnets : subnet.name => google_compute_subnetwork.subnet[subnet.name].name
     if var.vpc_config.subnets != null
